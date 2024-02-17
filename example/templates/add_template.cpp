2c8775207e426ac1ebe80f1eaf51efa105e38bdf
Lennox Fu <157094424+Lenoplus42@users.noreply.github.com>
2024-02-16 18:59:02 -0800

    Update CALICOncatenation_template.cpp

--- example/templates/add_template.cpp
+++ example/templates/add_template.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 /**
- * Return the sum of A and B.
- * 
- * A: a non-negative integer
- * B: another non-negative integer
+ * Return the CALICOncatenation of A.
+ *
+ * A: a string of representing a single word
  */
-int solve(int A, int B) {
+
+int solve(string A) {
     // YOUR CODE HERE
     return -1;
 }
@@ -17,8 +16,8 @@ int main() {
     int T;
     cin >> T;
     for (int i = 0; i < T; i++) {
-        int A, B;
-        cin >> A >> B;
-        cout << solve(A, B) << '\n';
+        string A;
+        cin >> A;
+        cout << solve(A) << '\n';
     }
 }
