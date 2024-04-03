0ed1210808d330adb766214a2b17fb3f66fe42ca
garimau <garimaupadhyay05@gmail.com>
2024-04-02 19:43:44 -0700

    added java sol

--- tntrelay/submissions/accepted/tntrelay.java
+++ tntrelay/submissions/accepted/tntrelay.java
@@ -9,7 +9,34 @@ class Solution {
      */
     static int solve(int A, int B, String C) {
         // YOUR CODE HERE
-        return -1;
+
+        /*
+        The intuition behind this solution is to use a sliding window
+        to find the maximum number of empty spaces in any segment of length B
+        */
+
+        int maximum_empty_spaces = 0;
+        int temp_empty_spaces = 0;
+
+        if (B >= A) {
+            return B;
+        }
+        
+        for (int i = 0; i < A; i++) {
+            if (i <= (B - 1)) {
+                if (C.charAt(i) == '-') {
+                    temp_empty_spaces += 1;
+                }
+            } else {
+                if (C.charAt(i) == '-') {
+                    temp_empty_spaces += 1;
+                } if (C.charAt(i - B - 1) == '-') {
+                    temp_empty_spaces -= 1;
+                }
+            }
+            maximum_empty_spaces = Math.max(maximum_empty_spaces, temp_empty_spaces);
+        }
+        return Math.max(0, B - maximum_empty_spaces + 1);
     }
     
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
