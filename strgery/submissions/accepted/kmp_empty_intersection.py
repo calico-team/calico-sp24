972bc9a8f5cd12e3fd9144aadc61366213ad2e0b
Gotheru <gotheru@gmail.com>
2024-04-16 16:11:28 +0200

    soy gay y del PSOE

--- strgery/submissions/accepted/kmp_empty_intersection.py
+++ strgery/submissions/accepted/kmp_empty_intersection.py
@@ -1,35 +1,3 @@
-from math import floor, log2
-
-
-class RMQ:
-    """
-    Implements Range Maximum Query
-    Indices in queries are inclusive
-    """
-
-    def __init__(self, v):
-        self.v = v
-        self.jmp = []
-        self.jmp.append([i for i in range(len(v))])
-        j = 1
-        while 1 << j <= len(v):
-            self.jmp.append([0 for _ in range(len(v) - (1 << j) + 1)])
-            for i in range(len(self.jmp[j])):
-                self.jmp[j][i] = self.cmb(self.jmp[j - 1][i], self.jmp[j - 1][i + (1 << (j - 1))])
-            j += 1
-
-    def cmb(self, a, b):
-        return min(a, b) if self.v[a] == self.v[b] else a if self.v[a] > self.v[b] else b
-
-    def index(self, l, r):
-        assert l <= r
-        d = int(floor(log2(r - l + 1)))
-        return self.cmb(self.jmp[d][l], self.jmp[d][r - (1 << d) + 1])
-
-    def query(self, l, r):
-        return self.v[self.index(l, r)]
-
-
 def compute_prefix_function(s: str) -> list[int]:
     """
     Computes the prefix array of the string s using KMP algorithm
@@ -61,7 +29,14 @@ def solve(P: str, S: str):
     back_important = back_prefix[n + 2:]
     back_important = back_important[::-1]
 
-    rmq = RMQ(back_important)
+    max_prefixes = [0 for _ in range(m)]
+    for i in range(1, m):
+        max_prefixes[i] = i if back_important[max_prefixes[i - 1]] < back_important[i] else max_prefixes[i - 1]
+
+    max_suffixes = [m - 1 for _ in range(m)]
+    for i in range(0, m - 1)[::-1]:
+        max_suffixes[i] = i if back_important[max_suffixes[i + 1]] < back_important[i] else max_suffixes[i + 1]
+
     assert m == len(fwd_important)
     for i in range(m):
         if fwd_important[i] == n:
@@ -72,14 +47,14 @@ def solve(P: str, S: str):
             continue
         if i + 1 < m:
             # Try the case [... prefix ... suffix ...]
-            j = rmq.index(i + 1, m - 1)
+            j = max_suffixes[i + 1]
             if fwd_important[i] + back_important[j] >= n:
                 # Solution found
                 print(f'{i - fwd_important[i] + 1} {fwd_important[i]} {j} {n - fwd_important[i]}')
                 return
         if i - m + fwd_important[i] >= 0:
             # Try the case [... suffix ... prefix ...]
-            j = rmq.index(0, i - m + fwd_important[i])
+            j = max_prefixes[i - m + fwd_important[i]]
             if fwd_important[i] + back_important[j] >= n:
                 # Solution found
                 print(f'{i - fwd_important[i] + 1} {fwd_important[i]} {j} {n - fwd_important[i]}')
