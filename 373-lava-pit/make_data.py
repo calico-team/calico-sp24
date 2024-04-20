427571ae3460ccced286795059702b0f6c671bae
Gotheru <gotheru@gmail.com>
2024-04-20 21:13:11 +0200

    pwease fix the judgehosts uwu

--- 373-lava-pit/make_data.py
+++ 373-lava-pit/make_data.py
@@ -21,6 +21,12 @@ generate the same thing every time. Seeds can be integers or strings.
 """
 SEED = 9034873104870
 
+max_T = 10
+max_N = 1000
+max_M = 1000
+max_total_N = 1000
+max_total_M = 1000
+
 
 class TestCase:
     """
@@ -30,11 +36,27 @@ class TestCase:
     TODO Change this to store the relevant information for your problem.
     """
 
-
     def __init__(self, N, M, G):
         self.N = N
         self.M = M
         self.G = G
+
+    def is_correct(self):
+        if not (1 <= self.N <= max_N):
+            return False
+        if not (1 <= self.M <= max_M):
+            return False
+        if len(self.G) != self.N:
+            return False
+        for i in range(self.N):
+            if len(self.G[i]) != self.M:
+                return False
+        for i in range(self.N):
+            for j in range(self.M):
+                if self.G[i][j] not in "LDO":
+                    return False
+        return True
+
 
 def make_sample_tests():
     """
@@ -50,18 +72,27 @@ def make_sample_tests():
     """
     G1 = ["LD", "DO"]
     G2 = ["OLD", "ODO", "DLO"]
+    G3 = [  # Minecraft case LOL
+        "OLOLO",
+        "LDOOD",
+        "DOLLO",
+        "OLODL",
+        "ODLOD",
+    ]
+    G4 = [  # Impossible case
+        "DLDL",  # haha grindr reference get it?
+        "LDDL",
+        "DOLL",
+        "LOLO",
+    ]
 
     main_sample_cases = [
         TestCase(2, 2, G1),
-        TestCase(3, 3, G2)
+        TestCase(3, 3, G2),
+        TestCase(5, 5, G3),
+        TestCase(4, 4, G4),
     ]
     make_sample_test(main_sample_cases, 'main')
-    
-    # bonus_sample_cases = [
-    #     TestCase(123456789, 987654321),
-    #     TestCase(3141592653589793238462643, 3832795028841971693993751),
-    # ]
-    # make_sample_test(bonus_sample_cases, 'bonus')
 
 
 def make_secret_tests():
@@ -75,25 +106,60 @@ def make_secret_tests():
     TODO Write sample tests. Consider creating edge cases and large randomized
     tests.
     """
-    def make_random_case(N, M):
+
+    weights = [
+        # [O, L, D]
+        [1, 1, 1],  # Equal probability
+        [10, 1, 1],  # A lot of obsidian
+        [1, 1, 10],  # Less obsidian and more diamonds
+        [3, 1, 3],  # A bit of lava
+        [3, 6, 9],  # TES 369 !!
+        [3, 1, 0],  # No diamonds but check impossible case!!
+    ]
+
+    def weights_to_name(w):
+        if w == weights[0]:
+            return 'equal probability'
+        elif w == weights[1]:
+            return 'tons_of_obsidian'
+        elif w == weights[2]:
+            return 'tons_of_diamonds'
+        elif w == weights[3]:
+            return 'bit_of_lava'
+        elif w == weights[4]:
+            return '333666999'
+        elif w == weights[5]:
+            return 'only_coal'
+
+    def make_random_case(N, M, w):
         G = []
         for i in range(N):
-            G.append("")
+            G.append('')
             for j in range(M):
-                block = random.randint(0, 2)
-                if block == 0:
-                    G[i] += "O"
-                elif block == 1:
-                    G[i] += "L"
-                else:
-                    G[i] += "D"
+                block = random.choices(['O', 'L', 'D'], weights=w)[0]
+                G[i] += block
         return TestCase(N, M, G)
-    
-    for i in range(5):
-        N_digit = random.randint(0, 100)
-        M_digit = random.randint(0, 100)
-        main_random_cases = [make_random_case(N_digit, M_digit) for _ in range(10)]
-        make_secret_test(main_random_cases, 'main_random')
+
+    # Make random small cases
+    for w in weights:
+        batch = []
+        total_N, total_M = 0, 0
+        for _ in range(max_T):
+            N = random.randint(1, min(max_N, max_total_N - total_N))
+            M = random.randint(1, min(max_M, max_total_M - total_M))
+            batch.append(make_random_case(N, M, w))
+            total_N += N
+            total_M += M
+            if total_N == max_total_N or total_M == max_total_M:
+                break
+        random.shuffle(batch)
+        make_secret_test(batch, f'main_random_small_{weights_to_name(w)}')
+
+    # Make big cases
+    for w in weights:
+        for _ in range(2):
+            batch = [make_random_case(max_N, max_M, w)]
+            make_secret_test(batch, f'main_random_big_{weights_to_name(w)}')
 
 
 def make_test_in(cases, file):
@@ -104,12 +170,13 @@ def make_test_in(cases, file):
     TODO Implement this for your problem.
     """
     T = len(cases)
+    assert 1 <= T <= max_T
     print(T, file=file)
     for case in cases:
         print(f'{case.N} {case.M}', file=file)
         for i in range(case.N):
-            str = case.G[i]
-            print(f'{str}', file = file)
+            print(case.G[i], file=file)
+        assert case.is_correct()
 
 
 def make_test_out(cases, file):
