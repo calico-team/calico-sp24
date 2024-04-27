4680596c8910f55867c9775f35d8614bd38dff7a
Albert Ye <y.a.728@berkeley.edu>
2024-04-26 22:00:48 -0700

    :3

--- amnesia/templates/amnesia_template.java
+++ amnesia/templates/amnesia_template.java
@@ -1,45 +1,43 @@
 import java.io.*;
 
 class Main {
-    private static String start() {
-        /*
-        Phase 1: Initialize N.
-        
-        Return a string of digits denoting the initial persistent value.
-        */
+    /**
+     * Phase 1: Initialize N.
+     * 
+     * Return a string of digits denoting the initial persistent value.
+     */
+    private static String start() { 
         // YOUR CODE HERE
-        return null; // change, depending on value
+        return "";
     }
 
-
+    /**
+     * Phase 2: Observe each brick.
+     * 
+     * N: a digit string denoting the persistent value from the previous run
+     * color: a letter ('B', 'S', or 'G') denoting the color of the current brick
+     * 
+     * Return a string of digits denoting the updated persistent value for the next
+     * run. This string must have the same length as the given N.
+     */
     private static String observe(String N, String color) {
-        /*
-        Phase 2: Observe each brick.
-        
-        N: a digit string denoting the persistent value from the previous run
-        color: a letter ('B', 'S', or 'G') denoting the color of the current brick
-        
-        Return a string of digits denoting the updated persistent value for the next
-        run. This string must have the same length as the given N.
-        */
         // YOUR CODE HERE
-        return null; // change, depending on value
+        return "";
     }
 
-
+    /**
+     * Phase 3: Submit the final answer.
+     * 
+     * N: a digit string denoting the persistent value from the previous run
+     * 
+     * Return a string of length 3 containing each of the characters 'B', 'S', and
+     * 'G' exactly once, denoting the types from least to most frequent. For
+     * example, if silver was the least frequent, gold was in the middle, and
+     * bronze was the most frequent, then you should output 'SGB'
+     */
     private static String answer(String N) {
-        /*
-        Phase 3: Submit the final answer.
-        
-        N: a digit string denoting the persistent value from the previous run
-        
-        Return a string of length 3 containing each of the characters 'B', 'S', and
-        'G' exactly once, denoting the types from least to most frequent. For
-        example, if silver was the least frequent, gold was in the middle, and
-        bronze was the most frequent, then you should output 'SGB'
-        */
         // YOUR CODE HERE
-        return null; // change, depending on value
+        return "";
     }
 
 
