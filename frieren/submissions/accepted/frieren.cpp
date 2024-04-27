8e08c3350533ea0ae66612508fd0921415fc784b
rohan19a <arohan19@gmail.com>
2024-04-26 20:39:42 -0700

    freemen

---
+++ frieren/submissions/accepted/frieren.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Return the number of times the person saw the Era Meteor Shower if it happens every fifty years.
+ * 
+ * Y: year the person was born
+ * L: the person's lifespan
+ * D: year the Demon King was slain and an Era Meteor Shower occurred.
+ */
+int solve(int Y, int L, int D) {
+    int born = ((Y - D) % 50 + 50) % 50;
+    int death = (born + L) % 50;
+    return L / 50 + (death <= born);
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int Y, L, D;
+        cin >> Y >> L >> D;
+        cout << solve(Y, L, D) << '\n';
+    }
+}
